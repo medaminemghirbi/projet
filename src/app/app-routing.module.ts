011298fc2421ff4cce4fb96cc73faea576267a7d
medaminemghirbi <mohamed.amine.mghirbi@gmail.com>
2022-06-14 15:57:22 +0200

    last updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { Page4Component } from './page4/page4.component';
+import { Page3Component } from './page3/page3.component';
+import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page2Component } from './page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Page1Component },
+  {path:'page2',component:Page2Component},
+  {path:'page3',component:Page3Component},
+  {path:'page4',component:Page4Component},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
